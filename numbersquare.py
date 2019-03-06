66843095d3a5f3f2425c4a4b25e1d3af0e490322
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-07 07:27:57 +0800

    add number square

---
+++ numbersquare.py
@@ -0,0 +1,14 @@
+# Squaring Number
+
+# count 2 square 5 with ** operator
+print(2**5)
+
+# count 2 square 5 with pow()
+print(pow(2,5))
+
+'''
+prototype:
+pow(x, y[, z]) -> number
+when we only put two parameter, this function will only process X ** y
+when we put three parameter, then the function will process (x ** y) % z
+'''
