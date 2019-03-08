62d376f9a147950633576c622b478c06004db99d
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-09 07:42:57 +0800

    add increment value function and decrement value function

---
+++ function-decrementthevalue.py
@@ -0,0 +1,8 @@
+# Decrement a Value Function
+def decrement(x, value=1):
+    x = x - value
+    return x
+
+b = 10
+print(decrement(b))
+print(decrement(b, 2))
---
+++ function-incrementthevalue.py
@@ -0,0 +1,8 @@
+# Increment a Value Function
+def increment(x, value=1):
+    x =+ value
+    return x
+
+a = 0
+print(increment(a))
+print(increment(a, 2))
