94f160db0b1b17e641fe1a05913c7db28cec025c
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-09 07:50:53 +0800

    function for return a few value

---
+++ function-returnafewofvalue.py
@@ -0,0 +1,14 @@
+# function for return a few of value
+def getvalues():
+    return 1, 2, 3
+
+# call the value
+a, b, c = getvalues()
+print(a)
+print(b)
+print(c)
+
+'''
+getvalues() will return the tuple object,
+remember we can also make the tuple without ()
+'''
