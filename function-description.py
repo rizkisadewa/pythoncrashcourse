60f031e37b401f0a8133f6c9d9ba49d6fda5ce81
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-10 07:56:10 +0800

    function description

---
+++ function-description.py
@@ -0,0 +1,18 @@
+# Function Description
+def multiple(a: float, b: float) -> float:
+    # description
+    '''
+    This function is for the multiple with two parameter value
+    Parameter List :
+    a : first value
+    b : second value
+
+    return type of function : float
+    '''
+
+    # function code
+    return a * b
+
+print(multiple(2,5))
+
+print(help(multiple))
