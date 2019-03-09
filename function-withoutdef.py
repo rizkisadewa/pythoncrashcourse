6f834fad5c3ce48cd41170ca62d7bde2fb3c0007
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-10 07:08:48 +0800

    function without def

---
+++ function-withoutdef.py
@@ -0,0 +1,7 @@
+# Make the function without keyword def, this one is anonymous function
+pangkat = lambda a,b: a**b
+print(pangkat(2,5))
+
+'''
+we do not specify the function name of the above, we set up the alias (pangkat) into the function which is lambda a,b: a**b
+'''
