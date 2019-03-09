3e148e0008770c116c2137e9ce368319be9702fa
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-10 07:05:56 +0800

    function alias

---
+++ function-alias.py
@@ -0,0 +1,12 @@
+# Long name for the function
+def contoh_nama_fungsi_sangat_panjang(lst):
+    for i in lst:
+        print(i, end=' ')
+
+contoh_nama_fungsi_sangat_panjang(['Python','Go','Nodejs','Java'])
+
+print()
+
+# make an alias and set into write
+write = contoh_nama_fungsi_sangat_panjang
+write(['Python','Go','Nodejs','Java'])
