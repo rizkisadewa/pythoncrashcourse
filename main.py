b497e897641e276798aa1a60631eb2038af38adf
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-22 07:21:17 +0800

    Importing all component from the other file

---
+++ main.py
@@ -0,0 +1,23 @@
+# importing all member of modul.py
+from modul import *
+
+def main():
+    # showing variable in modul
+    print(myvar)
+
+    # calling function in modul
+    myfunc()
+
+    # make an object from class A
+    # inside modul.py
+    a = A()
+
+    # calling mymethod() through object a
+    a.mymethod()
+
+if __name__ == '__main__':
+    main()
+
+'''
+* means all of component imported in this file
+'''
---
+++ modul.py
@@ -0,0 +1,12 @@
+# declaration variable
+myvar = 13
+
+# function
+def myfunc():
+    print("Fungsi myfunc() called ...")
+
+# class
+class A:
+    def __init__(self): pass
+    def mymethod(self):
+        print('Method A.mymethod() called ...')
