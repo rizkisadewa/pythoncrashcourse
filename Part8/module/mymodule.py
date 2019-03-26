4a08f5fcb8b7a0d01442d552b2cac4b72df12370
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-27 07:26:23 +0800

    'cleaning'

---
+++ Part8/module/mymodule.py
@@ -0,0 +1,9 @@
+# This is module
+a = 13
+
+def tambah(a, b):
+    return a + b
+
+def kali(a, b):
+    return a * b
+
---
+++ Part8/module/myprogram.py
@@ -0,0 +1,11 @@
+# The Program will execute the module in file mymodule.py
+import mymodule as m
+
+
+def main():
+    print('Nilai A adalah %d' % m.a)
+    print('2 + 3 = %d' % m.tambah(2, 3))
+    print('2 x 3 = %d' % m.kali(2, 3))
+
+if __name__ == '__main__':
+    main()
